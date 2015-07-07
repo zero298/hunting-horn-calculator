1f467ab2c877fae1d1f5dcfdc7e33419592c1e02
Stephen K <zero298@gmail.com>
2015-07-06 23:02:19 -0500

    Finished adding note data from MH4U
    Added the note data from MH4U

--- js/find_notes.js
+++ js/find_notes.js
@@ -1,120 +1,230 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var availableSongChart = document.getElementById("availableSongChart");
-    var clearButton = document.getElementById("clearNotes");
+(function () {
 
-    var selectedNotesArr = [];
-
-    function createNoteListener(noteButton, callback) {
-        noteButton.addEventListener("click", function (e) {
-            if (selectedNotesArr.indexOf(e.target.dataset.color) <= -1) {
-                selectedNotesArr.push(e.target.dataset.color);
-                if (callback) {
-                    callback();
-                }
-            }
-        });
-    }
-
-    function findSongs(givenNotes, setList) {
-
-        var possibleSongs = [];
-        var viableSong = false;
-
-        if (setList && setList["songs"]) {
-
-            var songs = setList["songs"];
-
-            for (var i = 0; i < songs.length; i++) {
-                viableSong = true;
-                var setListSongNotes = songs[i]["notes"];
-
-                for (var j = 0; j < givenNotes.length; j++) {
-                    if (setListSongNotes.indexOf(givenNotes[j]) <= -1) {
-                        viableSong = false;
-                        break;
-                    }
-                }
-
-                if (viableSong) {
-                    possibleSongs.push(songs[i]);
-                }
-            }
-        }
-        return possibleSongs;
-    }
-
+    /**
+     * Function to generate a song element that details the song name, duration, and notes needed
+     * @param {type} song
+     * @returns {Element}
+     */
     function createSongElement(song) {
-        var songElement = document.createElement("div");
-        var songTitleElement = document.createElement("span");
-        var songDurationElement = document.createElement("span");
-        var songNotesElement = document.createElement("span");
+        // Create all the elements of a song
+        var
+                songElement = document.createElement("div"),
+                songTitleElement = document.createElement("span"),
+                songDurationElement = document.createElement("span"),
+                songNotesElement = document.createElement("span"),
+                noteElement = null;
 
+        // Add the sub elements to the song element
         songElement.appendChild(songTitleElement);
         songElement.appendChild(songDurationElement);
         songElement.appendChild(songNotesElement);
 
-        songTitleElement.innerHTML = song.songName;
-        songDurationElement.innerHTML = song.duration;
+        // Set the innerHTML of what we know
+        songTitleElement.innerHTML = song.firstSong;
+        songDurationElement.innerHTML = song.firstDuration;
 
+        // Set the classes of the elements
         songElement.className = "noteResults";
         songTitleElement.className = "noteResultsTitle";
         songDurationElement.className = "noteResultsDuration";
         songNotesElement.className = "noteResultsNotes";
 
+        // Iterate over each element in the song
         song.notes.forEach(function (note) {
-            var noteElement = document.createElement("span");
+            // Create a note element
+            noteElement = document.createElement("span");
+            // Add the class name for the color
             noteElement.className = "note" + " " + note.toLowerCase();
+            // Add the note to the song
             songNotesElement.appendChild(noteElement);
         });
 
+        // Return the constructed song element
         return songElement;
     }
 
-    function printSongs(availableSongs) {
+    /**
+     * Find the possible songs that can be played by the given notes
+     * @param {Array} givenNotes
+     * @param {Array} allSongs
+     * @returns {Array}
+     */
+    function findSongs(givenNotes, allSongs) {
 
-        if (availableSongs) {
+        // Songs that can be played with the notes given
+        var possibleSongs = [];
+        // Whether the currently analyzed song is viable
+        var viableSong = false;
 
+        // Iterate all the available songs
+        for (var i = 0; i < allSongs.length; i++) {
+            // Consider the song viable until it isn't
+            viableSong = true;
 
-            availableSongChart.innerHTML = "";
+            // Keep a shortcut to the current song notes
+            var setListSongNotes = allSongs[i].notes;
 
-            if (!availableSongs.length) {
-                availableSongChart.innerHTML = "No Available Songs";
+            // Iterate over all the notes in the song
+            for (var j = 0; j < setListSongNotes.length; j++) {
+                // If we have not selected that note
+                if (givenNotes.indexOf(setListSongNotes[j]) <= -1) {
+                    // The song isn't viable
+                    viableSong = false;
+                    break;
+                }
             }
-            else {
-                availableSongs.forEach(function (viableSong) {
-                    var songDiv = createSongElement(viableSong);
-                    availableSongChart.appendChild(songDiv);
-                });
+
+            // If the song is viable
+            if (viableSong) {
+                // Add it to our list of viable songs
+                possibleSongs.push(allSongs[i]);
             }
         }
-        else {
-            availableSongChart.innerHTML = "Please select notes";
-        }
 
+        // Return the viable songs
+        return possibleSongs;
     }
 
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-
-            var data = JSON.parse(xhr.responseText);
-
-            var noteButtons = document.querySelectorAll("#selectableNotes button.note");
-
-            for (var i = 0; i < noteButtons.length; i++) {
-                createNoteListener(noteButtons[i], function (b) {
-                    var availableSongs = findSongs(selectedNotesArr, data);
-                    printSongs(availableSongs);
-                });
+    /**
+     * Function to load a set list
+     * @param {Function} loadCallback The function to call with the loaded data
+     * @returns {undefined}
+     */
+    function loadSetList(loadCallback) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var setListData = JSON.parse(xhr.responseText);
+                loadCallback(setListData);
             }
+        });
+        xhr.open("GET", "res/json/note_data.json");
+        xhr.send();
+    }
 
-            clearButton.addEventListener("click", function () {
-                selectedNotesArr.splice(0, selectedNotesArr.length);
-                printSongs();
+    /**
+     * Class that encapsulates the Hunting Horn Utility
+     * @param {type} selectableNotesEl
+     * @param {type} availableSongsEl
+     * @returns {find_notes_L1.HuntingHornUtil}
+     */
+    function HuntingHornUtil(selectableNotesEl, availableSongsEl) {
+        this.selectableNotesEl = selectableNotesEl;
+        this.availableSongsEl = availableSongsEl;
+
+        this.fullSongList = [];
+        this.selectedNotes = [];
+    }
+    HuntingHornUtil.prototype = {
+        /**
+         * Have the hunting horn utility set up its elements from set list
+         * @param {type} setList
+         * @returns {undefined}
+         */
+        initFromSetList: function (setList) {
+            this.fullSongList = setList.songs;
+            this.generateNoteButtons(setList.availableColors);
+        },
+        /**
+         * Clear the note selection
+         * @returns {undefined}
+         */
+        clearSelection: function () {
+            this.selectedNotes.splice(0, this.selectedNotes.length);
+        },
+        /**
+         * Print the available songs that can be played
+         * @param {type} availableSongs
+         * @returns {undefined}
+         */
+        printSongs: function (availableSongs) {
+            if (availableSongs) {
+                this.availableSongsEl.innerHTML = "";
+                if (!availableSongs.length) {
+                    this.availableSongsEl.innerHTML = "No Available Songs";
+                }
+                else {
+                    availableSongs.forEach(function (viableSong) {
+                        var songDiv = createSongElement(viableSong);
+                        this.availableSongsEl.appendChild(songDiv);
+                    }, this);
+                }
+            }
+            else {
+                this.availableSongsEl.innerHTML = "Please select notes";
+            }
+        },
+        /**
+         * Create a button, add its properties and add it to the container
+         * @param {type} color
+         * @returns {undefined}
+         */
+        instantiateNoteButton: function (color) {
+            var
+                    hhUtil = this,
+                    noteButton = document.createElement("button");
+
+            noteButton.className = ("note" + " " + color.color.replace(/\s/g, "").toLowerCase());
+            noteButton.title = (color.color + " " + "Note");
+            noteButton.value = (color.color);
+            noteButton.dataset.color = (color.color);
+
+            noteButton.addEventListener("click", function (noteClick) {
+                if (hhUtil.selectedNotes.indexOf(noteClick.target.dataset.color) <= -1) {
+                    hhUtil.selectedNotes.push(noteClick.target.dataset.color);
+                }
+
+                var availableSongs = findSongs(hhUtil.selectedNotes, hhUtil.fullSongList);
+                hhUtil.printSongs(availableSongs);
             });
-        }
-    });
 
-    xhr.open("GET", "/horn/res/json/note_data.json");
-    xhr.send();
-});
+            this.selectableNotesEl.appendChild(noteButton);
+        },
+        /**
+         * Function to generate clickable notes
+         * @param {type} availableColors
+         * @returns {undefined}
+         */
+        generateNoteButtons: function (availableColors) {
+            availableColors.forEach(this.instantiateNoteButton, this);
+        }
+    };
+    HuntingHornUtil.constructor = HuntingHornUtil;
+
+    /**
+     * Function to load the app
+     * @returns {undefined}
+     */
+    function bootstrap() {
+        var
+                availableGamesSelect = null,
+                selectableNotesContainer = null,
+                availableSongContainer = null,
+                clearNotesButton = null,
+                hhUtil = null;
+
+        // Get all our elements
+        availableGamesSelect = document.getElementById("gameSelect");
+        selectableNotesContainer = document.getElementById("selectableNotes");
+        availableSongContainer = document.getElementById("availableSongChart");
+        clearNotesButton = document.getElementById("clearNotes");
+
+        // Create our util
+        hhUtil = new HuntingHornUtil(selectableNotesContainer, availableSongContainer);
+
+        // Go and get our set list file
+        loadSetList(function (setList) {
+            // Initialize our util with the set list
+            hhUtil.initFromSetList(setList);
+        });
+
+        // Add ability to clear the notes
+        clearNotesButton.addEventListener("click", function () {
+            hhUtil.clearSelection();
+            hhUtil.printSongs();
+        });
+    }
+
+    // Wait for our page to load before running our app
+    document.addEventListener("DOMContentLoaded", bootstrap);
+}());
